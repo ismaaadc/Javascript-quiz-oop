9335bf2fd6b96a067dc8fd374eeebe765d85c707
Ismael <84606443+ismaaadc@users.noreply.github.com>
2022-10-22 13:16:58 +0200

    Questions

---
+++ data/data.js
@@ -0,0 +1,33 @@
+export const data = [
+    {
+        question: '¿Cuál de los siguientes opciones no es un editor de código?',
+        choises: [
+            'vim', 
+            'vscode', 
+            'word',
+            'emacs'
+        ],
+        answer: 'word'
+    },
+    {
+        question: '¿Qué lenguaje no es orientado a objetos?',
+        choises: [
+            'java', 
+            'Haskell', 
+            'C++',
+            'Python'
+        ],
+        answer: 'Haskell'
+    },
+    {
+        question: '¿Qué lenguaje no sirve para estilizar sitios web?',
+        choises: [
+            'stylus', 
+            'less', 
+            'sass',
+            'PostCSS',
+            'NextCSS'
+        ],
+        answer: 'PostCSS'
+    }
+]
